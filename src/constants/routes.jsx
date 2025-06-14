d6e52fbe59459b5076556bf3131277b1a76bd3d5
Maria Julieta Vignoli <Maria.Vignol@alu.frt.utn.edu.ar>
2025-06-14 17:45:26 -0300

    Realice la lógica del login y register

--- src/constants/routes.jsx
+++ src/constants/routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootView from "../views/routing/RootView";
 import HomeView from "../views/HomeView";
+import LoginView from "../views/LoginView";
+import RegisterView from "../views/RegisterView";
+
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +13,14 @@ export const router = createBrowserRouter([
       {
         path: "",
         element: <HomeView />,
+      },
+      {
+        path: "login",
+        element: <LoginView />,
+      },
+      {
+        path: "register",
+        element: <RegisterView />,
       },
     ],
   },
